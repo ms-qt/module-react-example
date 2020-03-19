a8868d956a5aaab0f8698ef35365733ff9fdff36
m_maohuawei <maohuawei@bdlbsc.com>
2020-03-19 10:03:26 +0800

    更新代码

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-                Hello World
-        </p>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>如果在下方能看见 test:: app_access_token,test:: user_access_token, test:: user_id ，说面成功运行插件</p>
+                    <p>
+                        {window.app_access_token}
+                    </p>
+                    <p>
+                        {window.user_access_token}
+                    </p>
+                    <p>
+                        {window.user_id}
+                    </p>
+
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
